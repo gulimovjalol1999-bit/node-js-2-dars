05557ca55d90a6bae18b114cf2e979dd283518b1
gulimovjalol1999-bit <gulimovjalol1999@gmail.com>
2026-01-20 16:40:45 +0500

    amaliyot

---
+++ server.js
@@ -0,0 +1,58 @@
+// const {add, minus} = require("./math")
+
+// console.log(add(10, 6));
+// console.log(minus(10, 6));
+
+// const fs = require("fs")
+
+// console.log(fs.mkdirSync("css"));
+
+const fs = require("fs");
+
+// fs.mkdir("__int_tests__", (error) => {
+//     if(error) console.log(error);
+// })
+
+// fs.mkdir("__int_tests__/__mocks__", (error) => {
+//     if(error) console.log(error);
+// })
+
+// fs.mkdir("__int_tests__/__tests__", (error) => {
+//     if(error) console.log(error);
+// })
+
+// fs.mkdir("coverage", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("dist", (err) => {
+//     if(err) console.log(err);
+// })
+
+// fs.mkdir("docs", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("docs/api", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("docs/public", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("scripts", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("scripts/deployment", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.mkdir("src", (err) => {
+//   if (err) console.log(err);
+// });
+
+// fs.appendFileSync("src/.gitignore", "salom")
+// fs.appendFileSync("src/index.js", "salom")
+// fs.appendFileSync("src/package.json", "salom")
